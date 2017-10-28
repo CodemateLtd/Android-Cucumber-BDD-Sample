2a82c1608ebf32267fbe15b760658f9dc5d4c2f4
Kenneth Toley <ktoley@gmail.com>
2017-10-28 14:55:54 -0700

    Run Config for Cucumber Tests

--- app/src/test/java/com/codemate/booklibrary/CucumberTests.java
+++ app/src/test/java/com/codemate/booklibrary/CucumberTests.java
@@ -8,6 +8,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources")
+@CucumberOptions(features = "app/src/test/resources")
 public class CucumberTests extends TestCase {
 }
